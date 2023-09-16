25edf738cb24740605a01b9e2781214c92dbea97
Alina <krakhmaalina@gmail.com>
2023-09-16 18:38:46 +0300

    start of layout

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ThemeProvider, createTheme } from '@mui/material'
 import App from './App.tsx'
 import './index.css'
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#3fbd3a',
+      contrastText: '#FFF',
+    },
+    text: {
+      primary: '#000',
+      secondary: '#9e9e9e'
+    },
+  },
+});
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
